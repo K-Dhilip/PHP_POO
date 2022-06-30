22d114882772af6cab3dfc5df509b31ef2b77a60
Dhilip KARTHIKEYAN <74380649+K-Dhilip@users.noreply.github.com>
2022-06-30 13:50:59 +0200

    Update

--- poo/00-cours.php
+++ poo/00-cours.php
@@ -1,18 +1,16 @@
 <!doctype html>
 <html lang="fr">
-
-<head>
+  <head>
     <title>Introduction - cours programmation orientée objet</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.2.0-beta1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-</head>
-
-<body>
+  </head>
+  <body>
 
     <div class="p-5 bg-light">
         <div class="container">
@@ -38,7 +36,7 @@
                     <li>Encapsuler</li>
                 </ul>
             </div> <!-- Fin de la col-12 -->
-
+            
             <div class="col-12">
                 <h2 class="text-center">Les avantages de la POO</h2>
                 <p>Le principal avantage de la POO est de faciliter la réutilisation d'un module. Ainsi on pourra reprendre les méthodes (fonctions) d'une classe existente dans une nouvelle classe grâce à la notion d'héritage.</p>
@@ -51,7 +49,7 @@
 
                 <p>Cette approche est tout de même moins intuitive que l'approche procédurale. La POO oblige en effet à réfléchir et modéliser avant de programmer.</p>
             </div><!-- Fin de la col-12 -->
-
+            
         </div>
     </div>
 
@@ -61,6 +59,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-</body>
-
+  </body>
 </html>
--- poo/01-objet.php
+++ poo/01-objet.php
@@ -1,18 +1,16 @@
 <!doctype html>
 <html lang="fr">
-
-<head>
-    <title>Introduction - cours programmation orientée objet</title>
+  <head>
+    <title>Objet - cours programmation orientée objet</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.2.0-beta1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-</head>
-
-<body>
+  </head>
+  <body>
 
     <div class="p-5 bg-light">
         <div class="container">
@@ -29,9 +27,9 @@
                 <p class="alert alert-secondary">
                     <strong>MaDate</strong> est une classe <br> <strong>$date1 = new MaDate:</strong> sont des objets ou bien des instanciations de cette classe.
                 </p>
-
+             
             </div> <!-- Fin de la col-12 -->
-
+            
             <div class="col-12">
                 <h2 class="text-center">Les propriétés</h2>
                 <p>Sur nos différents objets (ou instanciations) nous allons avoir des propriétés, des variables. Un objet pourra contenir plusieurs propriétés mais aussi plusieurs méthodes: sur une classe MaDate on pourra imaginer des propriétés pour afficher le nombre de jours mais aussi des méthodes pour ajouter des jours ou des mois.</p>
@@ -41,7 +39,7 @@
                 <h2 class="text-center">Les méthodes</h2>
                 <p>Comme nous en avons parlé au-dessus, les classes peuvent aussi contenir des méthodes. Ces dernières sont en effet des fonctions, appelées méthodes dans le cadre de la POO. Elles ont donc la même nomenclature qu'en procédural : <code>nomMethode();</code></p>
             </div><!-- Fin de la col-12 -->
-
+            
         </div>
     </div>
 
@@ -51,6 +49,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-</body>
-
+  </body>
 </html>
