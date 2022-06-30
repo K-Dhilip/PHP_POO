6ff83239735074fbd29b1bae2f97ba7e4930b6d4
Dhilip KARTHIKEYAN <74380649+K-Dhilip@users.noreply.github.com>
2022-06-30 09:12:56 +0200

    COMMIT

---
+++ poo/00-cours.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Introduction</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="p-5 bg-light">
+        <div class="container">
+            <h1 class="display-3">PDO : La programmation orientee object</h1>
+            <p class="lead">Introduction</p>
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">
+                    Historique
+                </h2>
+                <p>la notion dobject a ete introduite dans les langes de programation en 1992 par le locale_get_display_language simulus cette facon de programmer a eveoluee avanr d etre populaire en 1983 zvec la sortie du langue oritene dont utillisation ressembe a celle du langage </p>
+                <p>
+                    cette nouvelle facon de programmer repond a plusier besoins
+                </p>
+                <ul>
+                    <li>Elle facille les changements importants dans le programme </li>
+                    <li>En cas de changements les langage elle facilite la mise a niveau</li>
+                </ul>
+                <p>
+                    la poo va premettre de faire face aux changements majeurs qui auurant pus demamder de repondre un programme de a to z . Mais elle va demander egalment
+                </p>
+                <ul>
+                    <li>
+                        modilier differnt
+                    </li>
+                    <li>modulariser</li>
+                    <li>encapuler</li>
+                </ul>
+            </div>
+
+
+            <div class="col-12">
+                <h2 class="text-center">
+                    les advantagfe de la poo
+                </h2>
+                <p>
+                    le principal avantage de la poo est de facilatiopt la reutilizatiopn dun module ainisi on porra regrzade fonction dune ,cqlses exitancsr dans une nouellz classe notion dheritage
+                </p>
+                <p>une autr chose facitiee pare loriente est la lecture du code enn effect on compadrance plus faclilent une instruction <code>->ajout</code> quèun if else des multiples condition la course . la force de cette facon de programm calque sur la realite physique.une classe sera par expmple renault .les objects ou instraction de cette seront $voiture, $voiture2. etc . object est donc la voiture cet object aura des clearstatcache . En conslusion les advantage de cette facon de programmer sont les suivants </p>
+
+            </div>
+            <ul>
+                <li>la facilition du travail collabration</li>
+                <li>la simplification de la maintaince</li>
+                <li>assouplissent de code</li>
+            </ul>
+            <p>
+                cette approche est tout de meme moins intuitive que lapporche procedurale . la poo obloge en effet a reflechir et modiliser avant de programmer
+            </p>
+
+            <!-- fin de col -->
+        </div>
+    </div>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ poo/01-object.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Introduction</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="p-5 bg-light">
+        <div class="container">
+            <h1 class="display-3">PDO : La programmation orientee object</h1>
+            <p class="lead">Introduction</p>
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">
+                    Que sont les objects
+                </h2>
+                <p>les objects ressemblent aux vraiables que lon contaob (string, integer , float ,array etc) mais ces derines ses dropts ectx ces tableau devient vite vite tres compeletes et si don notre code, les person qui ne pas epas facilement lorque occupe des dates elles sont effet considers comme des ajouts ,,devient tres complication. Il faudra convepir il faudrea dates se beaucoup plus sample </p>
+                <p class="alret alert-secondary"></p>
+
+
+
+            </div>
+
+
+            <div class="col-12">
+                <h2 class="text-center">
+                    les advantagfe de la poo
+                </h2>
+                <p>
+                    le principal avantage de la poo est de facilatiopt la reutilizatiopn dun module ainisi on porra regrzade fonction dune ,cqlses exitancsr dans une nouellz classe notion dheritage
+                </p>
+                <p>une autr chose facitiee pare loriente est la lecture du code enn effect on compadrance plus faclilent une instruction <code>->ajout</code> quèun if else des multiples condition la course . la force de cette facon de programm calque sur la realite physique.une classe sera par expmple renault .les objects ou instraction de cette seront $voiture, $voiture2. etc . object est donc la voiture cet object aura des clearstatcache . En conslusion les advantage de cette facon de programmer sont les suivants </p>
+
+            </div>
+            <div class="col-12">
+                <h2 class="text-center">
+les methodes                </h2>
+                <p>
+                    comme nous avons parle au dessus, les class 
+                </p>
+                <p>une autr chose facitiee pare loriente est la lecture du code enn effect on compadrance plus faclilent une instruction <code>nomMethode();</code> </p>
+
+            </div>
+
+
+            <!-- fin de col -->
+        </div>
+    </div>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+
+</html>
